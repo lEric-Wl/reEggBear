0f908e78b3de2630dd414e10c3149a6c71d3024a
lEric-Wl <eric.wang887@gmail.com>
2025-06-25 19:15:07 -0400

    Created main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+import discord
+from discord import Intents
+from discord.ext import commands
+import requests
+import random
+import json
+
+'''
+with open('creds.json','r') as saves:
+		creds = json.load(saves)
+		saves.close()
+
+discordToken = creds['discordToken']
+'''
+discordToken = 'MTM4NzU2OTAyNTUzMzM0NTg4Mg.GEd4bK.98c8gz5R4Y802GNROcJsni0F4RbVJZ7ghtnBtM'
+intent = discord.Intents.all()
+bot = discord.Bot(intents = intent)
+allCogs = ['lottery']
+
+@bot.event
+async def on_ready():
+	print("Ready\n----------------------------")
+	
+@bot.command(description = 'Not for you to use')
+async def reload(ctx, extension = None):
+	#Reloads all of the cogs so that the bot does not need to be rebooted to update the code
+	
+	await ctx.defer(ephemeral = True);
+	
+	if not await bot.is_owner(ctx.author):
+		await ctx.respond('I told you in the description this is not for you to use, you Dingus!')
+		return
+	
+	try:
+		if extension is not None:
+			bot.reload_extension(f'cogs.{extension}')
+		else:
+			for stuff in allCogs:
+				bot.reload_extension(f'cogs.{stuff}')
+		print("\n\n\n\n\n\n\n\n\n\nreloaded")
+		
+		await ctx.respond('Done',ephemeral = True)
+		
+	except discord.ExtensionError as e:
+		await ctx.respond(f"Error: {str(e)}", ephemeral=True)	
+	except Exception as e:
+		await ctx.respond(f"Unexpected error: {str(e)}", ephemeral=True)
+  
+@bot.command()
+async def ping(ctx):
+	await ctx.respond(f"Pong! Latency is {bot.latency}")
+	print("recived")
+
+for stuff in allCogs:
+	bot.load_extension(f'cogs.{stuff}')	
+	
+bot.run(discordToken)
+
