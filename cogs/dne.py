b48717409ad9856de6e99bd9619ab36a25040cec
lEric-Wl <eric.wang887@gmail.com>
2025-07-23 09:41:08 -0400

    tried to simplify dne

--- cogs/dne.py
+++ cogs/dne.py
@@ -13,58 +13,46 @@ class View(discord.ui.View):
         
 
     @discord.ui.button(label="Attack!", style=discord.ButtonStyle.red, custom_id="attack")
-    async def attack(self, button, interaction):
-        self.bot.file_manager.load_saves()
-    
+    async def attack(self, button, interaction):        
+        #await interaction.response.defer()
+        self.bot.file_manager.load_saves() #Makes sure the file is ready
         message = str(interaction.message.id)
-        saves = self.bot.file_manager.saves
-    
-        monsters = saves.setdefault('dne', {}).setdefault('monsters', {})
-        if message not in monsters:
-            monsters[message] = random.randint(1200, 1750)
-            self.bot.file_manager.save_saves()
-    
+        if message not in self.bot.file_manager.saves['dne']['monsters']: #Adds monster to the file if it is new
+            self.bot.file_manager.saves['dne']['monsters'][message] = random.randint(1200,1750) 
+            self.bot.file_manager.save_saves() #Saves it. Save saves also reloads the file
+
         timestamp = int(time.time())
         member = str(interaction.user.id)
         attack = random.randint(150, 250)
-        coupon = random.randint(100, 250)
-    
+        coupons = random.randint(100, 250)
+
         self.bot.file_manager.check_member(member)
-    
-        timer = saves.setdefault('timer', {}).setdefault('dne', {})
-    
-        if timer.get(member, 0) <= timestamp:
+        timer = self.bot.file_manager.saves['timer']['dne']
+
+        if member not in timer or timer[member] <= timestamp:
             print('attacked')
-            timer[member] = timestamp + 900  # 15 min cooldown
-            monsters[message] -= attack
-            saves['members'][member][0] += coupon
+            self.bot.file_manager.saves['timer']['dne'][member] = timestamp + 900 #15 min
+            self.bot.file_manager.saves['dne']['monsters'][message] -= attack
+            self.bot.file_manager.saves['members'][member][0] += coupons
             self.bot.file_manager.save_saves()
-    
-            await interaction.response.send_message(
-                f'Glory be! <@{member}> smote the {self.monster} for {attack} HP, and gained {coupon} coupons!'
-            )
-    
-            if monsters[message] <= 0:
-                saves['members'][member][0] += 500
-                await interaction.followup.send(
-                    f'Huzzah! The {self.monster} has been vanquished by <@{member}>! '
-                    f'For dealing the final blow, <@{member}> has earned an additional 500 coupons.'
-                )
-                del monsters[message]
+            await interaction.response.send_message(f'Glory be! <@{member}> smote the {self.monster} for {attack} HP, and gained {coupons} coupons!')
+
+            if self.bot.file_manager.saves['dne']['monsters'][message] <= 0:
+                self.bot.file_manager.saves['members'][member][0] += 500
+                await interaction.followup.send(f'Huzzah! The {self.monster} has been vanquished by <@{member}>! For dealing the final blow, <@{member}> has earned an additional 500 coupons.')
+                del self.bot.file_manager.saves['dne']['monsters'][message]
                 await interaction.message.delete()
-                self.bot.file_manager.save_saves()
         else:
-            cooldown = timer[member]
-            await interaction.response.send_message(
-                f"You're still on cooldown. Try again in <t:{cooldown}:R>", ephemeral=True
-            )
+            await interaction.response.send_message(f"You're still on cooldown. Try again in <t:{self.bot.file_manager.saves['timer']['dne'][member]}:R>",ephemeral=True)
             print('failed')
+        self.bot.file_manager.save_saves()
         return
+    
     
 class DNE(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-
+        self.monster.start()
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -75,8 +63,9 @@ class DNE(commands.Cog):
         datetime.time(hour=13, minute=0, tzinfo=datetime.timezone.utc),
         datetime.time(hour=21, minute=0, tzinfo=datetime.timezone.utc)
     ])
-    async def monster(self,ctx):
-        await ctx.respond(f'<@&1387579469749030934> warriors! A new foe approaches. Band together to defeat it!')
+    async def monster(self):
+        channel = self.bot.get_channel(1397568088622235750)
+        await channel.send(f'<@&1387579469749030934> warriors! A new foe approaches. Band together to defeat it!')
 
         monster = random.choice(['mike'])
         image = discord.File(f'{monster}.png', filename='image.png')
@@ -86,7 +75,7 @@ class DNE(commands.Cog):
             description='Whach this monster to make him drop coupons from his hoard.'
         )
         embed.set_image(url='attachment://image.png')
-        await ctx.send(embed=embed, file=image, view=View(self.bot,monster))
+        await channel.send(embed=embed, file=image, view=View(self.bot,monster))
         return
 
 def setup(bot):
