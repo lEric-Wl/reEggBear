45b7eb7da9b25d8520f3c8b316fb102c54d54b6a
lEric-Wl <eric.wang887@gmail.com>
2025-07-30 16:34:15 -0400

    went back to math.js

--- cogs/counting.py
+++ cogs/counting.py
@@ -31,10 +31,20 @@ class Counting(commands.Cog):
                 await message.channel.send(f"Wrong number! The next number should be {self.bot.file_manager.saves['counting'][0] + 1}.")
                 self.bot.file_manager.saves['counting'] = [0, None]
         else:
-            try:
-                print(eval(message.content))
-            except Exception as e:
+            response = requests.get("httpsd://api.mathjs.org/v4/", params={"expr": message.content})
+            if response.status_code != 200:
                 return
+            if not response.text.isdigit():
+                return 
+            number = int(response.text)
+            if number == self.bot.file_manager.saves['counting'][0] + 1:
+                self.bot.file_manager.saves['counting'] = [number,message.author.id]  
+                await message.add_reaction('✅')
+            else:
+                await message.add_reaction('❌')
+                await message.channel.send(f"Wrong number! The next number should be {self.bot.file_manager.saves['counting'][0] + 1}.")
+                self.bot.file_manager.saves['counting'] = [0, None]
+            
         self.bot.file_manager.save_saves()
 
 def setup(bot):
